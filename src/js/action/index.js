45483198d5c060a2a8e30715d2ce15284d90b1e9
Harut Muqoyan <hmuqoyan@gmail.com>
2019-07-28 22:07:42 +0400

    for demo

---
+++ src/js/action/index.js
@@ -0,0 +1,16 @@
+import { ADD, SUBTRACT, DIVIDE } from '../const/index';
+
+
+export function Add() {
+    return {type: ADD};
+}
+
+export function Subtract() {
+    return {type: SUBTRACT};
+}
+
+export function Divide() {
+    return {type: DIVIDE};
+}
+
+
